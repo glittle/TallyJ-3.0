5d0850dc6c94ef5094aa48ea3ab4f1de0b80dd61
Glen Little <glen.little@gmail.com>
2019-07-01 21:41:09 -0600

    upgrade libraries; start adding login

--- Site/OwinStartup.cs
+++ Site/OwinStartup.cs
@@ -1,17 +1,21 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using static System.Configuration.ConfigurationManager;
 
-[assembly: OwinStartup(typeof (TallyJ.OwinStartup))]
+[assembly: OwinStartup(typeof(TallyJ.OwinStartup))]
 
 namespace TallyJ
 {
-  public class OwinStartup
-  {
-    public void Configuration(IAppBuilder app)
+    public class OwinStartup
     {
-      // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
 
-      app.MapSignalR();
+            app.MapSignalR();
+
+            app.UseGoogleAuthentication(AppSettings["google-ClientId"], AppSettings["google-ClientSecret"]);
+            app.UseFacebookAuthentication(AppSettings["facebook-AppId"], AppSettings["facebook-AppSecret"]);
+        }
     }
-  }
 }
