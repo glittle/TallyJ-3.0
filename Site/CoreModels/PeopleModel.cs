85f29fd759f4f91a8dd189c853113eba84e2a377
Glen Little <glen.little@gmail.com>
2025-03-09 22:27:31 -0600

    minor tweaks

--- Site/CoreModels/PeopleModel.cs
+++ Site/CoreModels/PeopleModel.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
-using CsQuery;
 using EntityFramework.Extensions;
 using TallyJ.Code;
 using TallyJ.Code.Enumerations;
